d7ad528ddbcd61b703dec792380a82eb8e372590
lvalenti <leonardo.valenti@eng.it>
2023-07-01 10:07:24 +0200

    first part

---
+++ src/app/quiz-maker/quiz-maker.component.ts
@@ -0,0 +1,60 @@
+import {Component, OnInit} from '@angular/core';
+import {Category, CategoryWithSub, Difficulty, Question} from '../data.models';
+import {Observable, map, mergeMap, of, switchMap, tap} from 'rxjs';
+import {QuizService} from '../quiz.service';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-quiz-maker',
+  templateUrl: './quiz-maker.component.html',
+  styleUrls: ['./quiz-maker.component.css']
+})
+export class QuizMakerComponent {
+
+  form: FormGroup;
+
+  categories$: Observable<CategoryWithSub[]>;
+  subcategories$: Observable<Category[] | undefined> | undefined;
+
+  questions$!: Observable<Question[]>;
+
+  constructor(private fb: FormBuilder, protected quizService: QuizService) {
+
+    this.form = this.fb.group({
+      mainCategory: [{value: '', disabled: false}, [Validators.required]],
+      subCategory: [{value: '', disabled: false}],
+      difficulty: [{value: '', disabled: false}, [Validators.required]],
+   });
+
+    this.categories$ = quizService.getAllCategories();
+
+    this.subcategories$ = this.form.get("mainCategory")?.valueChanges.pipe(
+      tap((mainCategory: CategoryWithSub)=>{
+        const subCategoryCtrl = this.form.get("subCategory");
+        subCategoryCtrl?.setValue("");
+        if (mainCategory.subcategories && mainCategory.subcategories.length>0) {
+          subCategoryCtrl?.addValidators(Validators.required);
+        }
+        else {
+          subCategoryCtrl?.clearValidators();
+        }
+        subCategoryCtrl?.updateValueAndValidity();
+      }),
+      map((mainCategory: CategoryWithSub) => mainCategory.subcategories)
+    );
+  }
+
+  createQuiz(): void {
+    if (this.form.invalid) {
+      console.log("form invalid", this.form);
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    const { mainCategory, subCategory, difficulty } = this.form.value;
+
+    debugger;
+
+    this.questions$ = this.quizService.createQuiz(mainCategory.id || subCategory.id, difficulty);
+  }
+}
